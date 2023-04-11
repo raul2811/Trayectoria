9629eb8e3b42c9cadf8a3f3665836c790c8311ba
raul2811 <raul.serrano01@up.ac.pa>
2023-04-10 22:25:25 -0500

    C

--- C/Año2/semestre1/test.cpp
+++ C/Año2/semestre1/test.cpp
@@ -1,7 +1,6 @@
-#include<iostream>//! Libreria prederminada C++.
-#include<iomanip>//! Libreria para asignar el rendodeo del punto decimal.
+#include <iostream>//! Libreria prederminada C++.
+#include <iomanip>//! Libreria para asignar el rendodeo del punto decimal.
 using namespace std;
-
 double  calculos(double a,int b) 
 {
     double multiplicacion;
@@ -15,15 +14,16 @@ int main()
 int x,num,horas;//Almacena El Numero De Empleados
 double  shoras,salariot;
 string cdg;//codigo de empleado
-cout<<"Ingrese La Cantidad De Empleados Que Desea Ingresar-"<<endl;
+cout<<"Ingrese La Cantidad De Empleados Que Desea Ingresar"<<endl<<"-";
 cin>>x;//capturamos el Valor
 for (num=0;num<x;num++)
 {
-cout<<"Ingrese El Codigo De Empleado"<<endl;
+cout<<"Empleado#"<<num+1<<"/"<<x<<endl;    
+cout<<"Ingrese El Codigo De Empleado"<<endl<<"-";
 cin>>cdg;
-cout<<"Ingrese El Salario Por Hora"<<endl;
+cout<<"Ingrese El Salario Por Hora"<<endl<<"-";
 cin>>shoras;
-cout<<"Ingrese El Numero De Horas Trabajadas"<<endl;
+cout<<"Ingrese El Numero De Horas Trabajadas"<<endl<<"-";
 cin>>horas;
 salariot=calculos(shoras,horas);
 cout<<"Al Empleado Con Codigo:#"<<cdg<<endl<<"Se Le Debe Pagar:$"<<fixed<<setprecision(2)<<salariot;
