bcc111932d0e505fbf8f98a5d67702047b20709a
raul2811 <raul.serrano01@up.ac.pa>
2023-08-30 23:53:07 -0500

    Java

--- Java/Edad.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import java.io.IOException;
-import java.util.Scanner;
-
-class Edad {
-    public static void main(String[] args) throws IOException { 
-        Scanner scanner = new Scanner(System.in);
-    }
-    }
