d749976f1d62bcfc121bd2075af0fc94535ffc07
raul2811 <raul.serrano01@up.ac.pa>
2023-09-04 00:19:40 -0500

    Python

---
+++ Jupyter/contrato.py
@@ -0,0 +1,14 @@
+#Variables
+#NOMBREM=Nombre de la modelo.
+#CEDULAM=Cedula de la modelo en letra y numero.
+#DIAS=Fecha en que se realizara la sesion.
+#HORAI=Hora de inicio.
+#HORAF=Hora de finalizacion.
+#DIAF=Dia de la firma del contrato.
+#CEDULAMN=Cedula en numero de la modelo.
+a=0
+print("Ingresa un numero")
+a=input("\n")
+
+if a == 1:
+    print ("Uno")
