924542f8832b53eb0ecc27a03ac16956baa66842
raul2811 <raul.serrano01@up.ac.pa>
2023-12-12 02:11:56 -0500

    Ejercicios

--- Python/Clases/Quiz/imprimir.py
+++
@@ -1,7 +0,0 @@
-def Formato(Palabra):
-    Decoracion=len(Palabra)
-    print("+"+"-"*int(Decoracion)+"+",end=("\n"),);
-    print("|"+str(Palabra)+"|");
-    print("+"+"-"*int(Decoracion)+"+")
-Palabra=(input("Ingrese Lo Que Desea Imprimir"));
-Formato(Palabra);
