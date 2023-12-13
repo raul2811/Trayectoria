f8a7de2ddfa58f5e2046b86ac360a85c10129270
raul2811 <raul.serrano01@up.ac.pa>
2023-12-13 04:45:49 -0500

    Refactor code to remove spaces from a string

--- Python/Ejercicios resueltos de Codewars u otros sitios similares/eliminar_espacios_cadena.py
+++ Python/Ejercicios resueltos de Codewars u otros sitios similares/eliminar_espacios_cadena.py
@@ -25,7 +25,48 @@ def no_space(x):
      return ''.join(x.split())
      
  método, split() divide la cadena en palabras, y luego join() une esas palabras sin ningún separador, 
- eliminando así los espacios entre ellas.   
+ eliminando así los espacios entre ellas.
+ 
+ def no_space(x):
+    # Inicializa una cadena vacía para almacenar los caracteres sin espacios
+    str_char = ''
+    
+    # Itera a través de cada índice en la cadena 'x'
+    for i in range(len(x)):
+        # Verifica si el carácter en la posición 'i' es un espacio
+        if x[i] == ' ':
+            # Si es un espacio, pasa al siguiente carácter
+            continue
+        else:
+            # Si no es un espacio, agrega el carácter a la cadena 'str_char'
+            str_char = str_char + x[i]
+    
+    # Retorna la cadena resultante sin espacios
+    return str_char 
+    
+-------------------------------------
+def no_space(x):
+    # Inicializa una cadena vacía para almacenar los caracteres sin espacios
+    str_char = ''
+    
+    # Itera a través de cada índice en la cadena 'x'
+    for i in range(len(x)):
+        # Verifica si el carácter en la posición 'i' es un espacio
+        if x[i] == ' ':
+            # Si es un espacio, pasa al siguiente carácter
+            continue
+        else:
+            # Si no es un espacio, agrega el carácter a la cadena 'str_char'
+            str_char = str_char + x[i]
+    
+    # Retorna la cadena resultante sin espacios
+    return str_char
+    
+Este código crea una cadena vacía str_char y recorre cada carácter en la cadena x. Si el 
+carácter actual es un espacio, se omite; de lo contrario, se agrega a la cadena str_char. Finalmente, 
+se devuelve la cadena str_char, 
+que contiene todos los caracteres de la cadena original x excepto los espacios.
+
 --------------------------------------
 
 """    
