ca14b0eedf5b5eb6438b7016b67c517474d4d513
raul2811 <raul.serrano01@up.ac.pa>
2024-02-02 10:50:50 -0500

    se elimino

--- Python/notion-intento/header.py
+++
@@ -1,7 +0,0 @@
-from read_token import token #** importa el token 
-
-headers = {
-    "Authorization": "Bearer " + token(),
-    "Content-Type": "application/json",
-    "Notion-Version": "2022-02-22"
-}
--- Python/notion-intento/read_token.py
+++
@@ -1,5 +0,0 @@
-def token (): #** Funcion para leer el token de un archivo de texto
-    abrir_txt=open('TOKEN.txt','r')
-    token = abrir_txt.read()
-    abrir_txt.close()
-    return token
